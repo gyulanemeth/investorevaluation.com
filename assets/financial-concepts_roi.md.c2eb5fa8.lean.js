5cab2cb141b0a76b0ab848f33f4052ae95604e88
Gyula Németh <gyula@chamaileon.io>
2023-04-10 12:52:42 +0200

    website update

---
+++ assets/financial-concepts_roi.md.c2eb5fa8.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,N as a}from"./chunks/framework.0799945b.js";const u=JSON.parse('{"title":"ROI","description":"","frontmatter":{},"headers":[],"relativePath":"financial-concepts/roi.md"}'),n={name:"financial-concepts/roi.md"},o=a("",9),i=[o];function r(c,l,p,h,d,m){return t(),s("div",null,i)}const v=e(n,[["render",r]]);export{u as __pageData,v as default};
